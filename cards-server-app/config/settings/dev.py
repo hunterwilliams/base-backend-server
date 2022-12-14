4d91e28fa9ffab81e43d2ca70c432abdb26530ad
Hunter Williams <hgw2es@virginia.edu>
2022-12-14 12:09:08 +0700

    add mailcatcher for sending emails locally

--- cards-server-app/config/settings/dev.py
+++ cards-server-app/config/settings/dev.py
@@ -18,3 +18,11 @@ DATABASES = {
 
 AWS_STORAGE_BUCKET_NAME = "NOT_SET_AWS_STORAGE_BUCKET_NAME"
 AWS_S3_CUSTOM_DOMAIN = "%s.s3.amazonaws.com" % AWS_STORAGE_BUCKET_NAME
+
+if DEBUG:
+    print("Dev Mode: Sending emails locally")
+    EMAIL_HOST = "mailcatcher"
+    EMAIL_HOST_USER = ""
+    EMAIL_HOST_PASSWORD = ""
+    EMAIL_PORT = 1025
+    EMAIL_USE_TLS = False
