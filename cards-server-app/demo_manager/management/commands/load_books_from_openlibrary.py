35c06df5c96905d9edbfdb421661b9553863ea58
Naveya Jintaya <naveya@divertise.asis>
2023-02-02 17:04:53 +0700

    Support load books from openlibrary to Book and BookWithIndex model.

--- cards-server-app/demo_manager/management/commands/load_books_from_openlibrary.py
+++ cards-server-app/demo_manager/management/commands/load_books_from_openlibrary.py
@@ -3,7 +3,7 @@ import json
 from django.contrib.auth import get_user_model
 from django.core.management.base import BaseCommand, CommandError
 
-from ...models import Author, Book
+from ...models import Author, Book, BookWithIndex
 
 User = get_user_model()
 
@@ -15,6 +15,7 @@ class Command(BaseCommand):
                             type=str,
                             help="file directory of openlibrary search response json format e.g. "
                                  "'https://openlibrary.org/search.json?q=a&limit=1000'")
+        parser.add_argument("--bookwithindex", action="store_true", help="Load with model BookWithIndex")
 
     @staticmethod
     def load_json_file(kwargs):
@@ -31,10 +32,15 @@ class Command(BaseCommand):
         return book_search_json
 
     def handle(self, *args, **kwargs):
+        is_model_bookwithindex = kwargs['bookwithindex']
         book_search_json = self.load_json_file(kwargs)
         book_results = book_search_json["docs"]
         imported_books = 0
+        book_class = BookWithIndex if is_model_bookwithindex else Book
+
         for book in book_results:
+            if book_class.objects.filter(title=book["title"]).exists():
+                continue
             authors = []
             for author_name in book.get("author_name", []):
                 author, _created = Author.objects.get_or_create(name=author_name)
@@ -44,8 +50,8 @@ class Command(BaseCommand):
                 isbn_list = book.get("isbn")
                 isbn = isbn_list[0] if isbn_list else ""
 
-                book = Book.objects.create(title=book["title"], isbn=isbn)
-                book.authors.add(*authors)
+                book_obj = book_class.objects.create(title=book["title"], isbn=isbn)
+                book_obj.authors.add(*authors)
                 imported_books += 1
 
         self.stdout.write(self.style.SUCCESS(
