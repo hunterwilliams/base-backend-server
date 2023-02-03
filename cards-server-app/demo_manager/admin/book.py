b743704a6a7dda870492b891fbdd30b6628e4d3f
Naveya Jintaya <naveya@divertise.asis>
2023-02-03 14:45:21 +0700

    Optimize database queries in Book admin page.

--- cards-server-app/demo_manager/admin/book.py
+++ cards-server-app/demo_manager/admin/book.py
@@ -1,25 +1,32 @@
-from django.contrib import admin
+from django.contrib.postgres.aggregates import StringAgg
 
 from config.helpers import get_all_field_names
+from import_export.admin import ImportExportModelAdmin
+
 
 from ..models import Author, Book
 
 
-class AuthorAdminView(admin.ModelAdmin):
+class AuthorAdminView(ImportExportModelAdmin):
     model = Author
     list_display = ("name", )
     search_fields = ("name", )
 
 
-class BookAdminView(admin.ModelAdmin):
+class BookAdminView(ImportExportModelAdmin):
     model = Book
     fields = get_all_field_names(Book)
     readonly_fields = ("id", "created_at", )
     list_display = ("id", "title", "author_names", )
-    search_fields = ("id", "title", "author_names", )
+    search_fields = ("id", "title", "authors", )
     autocomplete_fields = ["authors"]
 
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        queryset = queryset.annotate(
+            author_names=StringAgg("authors__name", delimiter=", ", ordering="authors__name"),
+        )
+        return queryset
+
     def author_names(self, book):
-        return ", ".join(list(book.authors.values_list('name', flat=True)))
-
-
+        return book.author_names
