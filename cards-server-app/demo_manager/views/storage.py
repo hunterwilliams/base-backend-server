f1abb4bc1a40724df928a604cdfdc14703556a54
Naveya Jintaya <naveya@divertise.asis>
2023-02-03 17:41:18 +0700

    Catch exeception user does not have book storage.

--- cards-server-app/demo_manager/views/storage.py
+++ cards-server-app/demo_manager/views/storage.py
@@ -1,22 +1,27 @@
 from django.contrib.auth import get_user_model
-from rest_framework import viewsets
-from rest_framework.permissions import AllowAny
+from rest_framework.mixins import ListModelMixin
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import GenericViewSet
 
 from config.mixins import PaginationWithEagerLoadingViewSetMixin
 
-from ..models import BookWithIndex
+from ..models import BookWithIndex, UserBookStorage
 from ..serializers import BookWithIndexSerializer
 
 User = get_user_model()
 
 
-class UserBookStorageViewSetV1(PaginationWithEagerLoadingViewSetMixin, viewsets.ReadOnlyModelViewSet):
-    permission_classes = (AllowAny,)
+class UserBookStorageViewSetV1(PaginationWithEagerLoadingViewSetMixin, ListModelMixin, GenericViewSet):
+    permission_classes = (IsAuthenticated,)
     serializer_class = BookWithIndexSerializer
     queryset = BookWithIndex.objects.none()
 
     OVERRIDE_PAGE_SIZE = 10
 
     def get_queryset(self):
-        self.queryset = self.request.user.book_storage.books
+        try:
+            self.queryset = self.request.user.book_storage.books
+        except UserBookStorage.DoesNotExist:
+            pass
+
         return super().get_queryset()
