aeebccd1c929c6f188de5609615eed3f2712cc6f
Naveya Jintaya <naveya@divertise.asis>
2023-02-06 17:11:12 +0700

    Send alert email to all super user.

--- cards-server-app/config/middleware.py
+++ cards-server-app/config/middleware.py
@@ -1,4 +1,6 @@
-from django.conf import settings as proj_settings
+from django.conf import settings
+from django.core import mail
+from django.template.loader import render_to_string
 from django.urls import resolve
 from django.urls.exceptions import Resolver404
 from django.utils import timezone
@@ -13,10 +15,10 @@ class SlowAPIAlertMiddleware:
         self.get_response = get_response
 
     def is_validate(self):
-        self.alert_namespaces = getattr(proj_settings, "SLOW_API_ALERT_NAMESPACES", None)
-        self.alert_at_ms = getattr(proj_settings, "SLOW_API_ALERT_AT_MS", None)
+        self.alert_namespaces = getattr(settings, "SLOW_API_ALERT_NAMESPACES", None)
+        self.alert_at_ms = getattr(settings, "SLOW_API_ALERT_AT_MS", None)
 
-        if not self.alert_namespaces or not self.alert_at_ms:
+        if not self.alert_at_ms or not self.alert_namespaces:
             return False
 
         return True
@@ -36,10 +38,37 @@ class SlowAPIAlertMiddleware:
 
         return resolver_match.namespaces and resolver_match.namespaces[-1] in self.alert_namespaces
 
+    def send_mail_to_staff(self, request, response, request_duration_ms, request_at, response_at):
+        from django.contrib.auth import get_user_model
+        User = get_user_model()
+
+        context = {
+            "alert_at_ms": self.alert_at_ms,
+            "request_duration_ms": request_duration_ms,
+            "request": {
+                "at": f"{request_at}",  # datetime with timezone
+                "method": request.method,
+                "url": request.path,
+            },
+            "response": {
+                "at": f"{response_at}",
+                "status_code": response.status_code,
+            },
+        }
+        recipient_list = User.objects.filter(is_superuser=True).values_list("email", flat=True)
+
+        mail.send_mail(
+            subject="[Alert] Slow API detected",
+            message=render_to_string("./email/slow_api_alert/slow_api_alert.txt", context),
+            from_email=getattr(settings, "DEFAULT_FROM_EMAIL", "noreply@divertise.asia"),
+            recipient_list=recipient_list,
+            html_message=render_to_string("./email/slow_api_alert/slow_api_alert.html", context),
+            fail_silently=False,
+        )
+
     def __call__(self, request):
         # Code to be executed for each request before
         # the view (and later middleware) are called.
-        print(f">>> MIDDLEWARE req: {request}")
         if not self.is_validate() or not self.is_api_alert_request(request):
             return self.get_response(request)
 
@@ -48,9 +77,10 @@ class SlowAPIAlertMiddleware:
 
         # Code to be executed for each request/response after
         # the view is called.
-        print(f">>> MIDDLEWARE response: {response}")
-        request_durations = timezone.now() - req_time
-        if request_durations.total_seconds() * 1000 >= self.alert_at_ms:
-            # TODO: Send email
-            print("!! Alert!!")
+        resp_time = timezone.now()
+        request_duration_ms = (resp_time - req_time).total_seconds() * 1000
+
+        if request_duration_ms >= self.alert_at_ms:
+            self.send_mail_to_staff(request, response, request_duration_ms, request_at=req_time, response_at=resp_time)
+
         return response
