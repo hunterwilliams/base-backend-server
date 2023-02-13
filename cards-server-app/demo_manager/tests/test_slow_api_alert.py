89fda7ad65b893f9b3b983e58f9d23abffc74d58
Naveya Jintaya <naveya@divertise.asis>
2023-02-13 09:43:39 +0700

    Create unittest does email sent when api slow response.

--- cards-server-app/demo_manager/tests/test_slow_api_alert.py
+++ cards-server-app/demo_manager/tests/test_slow_api_alert.py
@@ -2,16 +2,21 @@
 # Demo Manager's Slow API Alert
 # """
 from unittest.mock import patch
+
+from django.contrib.auth import get_user_model
 from django.core import mail
 from django.test import override_settings
 from rest_framework.reverse import reverse
 
 from config.helpers import BaseTestCase
-from demo_manager.models import Author, Book
+
+User = get_user_model()
 
 
 class TestSlowApiAlertTriggered(BaseTestCase):
-
+    def setUp(self):
+        super().setUp()
+        self.staff_user = User.objects.create(email="staffuser@mockup.test", is_superuser=True)
 
     def get_book_list_first_page(self):
         self.given_url(reverse(f"demo:books-list"))
@@ -21,7 +26,8 @@ class TestSlowApiAlertTriggered(BaseTestCase):
 
     @patch("config.middleware.slow_api.slow_api_alert_triggered.send")
     @override_settings(SLOW_API_ALERT_AT_MS=30000)
-    def test_signal_not_triggered_when_get_book_list_with_valid_page_time_to_response_as_expected(self, mock_signal_send):
+    def test_signal_not_triggered_when_get_book_list_with_valid_page_time_to_response_as_expected(self,
+                                                                                                  mock_signal_send):
         self.get_book_list_first_page()
 
         self.assertResponseSuccess()
@@ -35,7 +41,6 @@ class TestSlowApiAlertTriggered(BaseTestCase):
 
         self.assertResponseSuccess()
         self.assertTrue(mock_signal_send.called)
-        print(f">>> mock_signal_send.call_args[1]: {mock_signal_send.call_args[1]}")
 
         print(">> test_signal_triggered_when_get_book_list_with_valid_page_took_too_much_time_to_response: OK <<")
 
@@ -47,11 +52,14 @@ class TestSlowApiAlertTriggered(BaseTestCase):
         self.assertEqual(len(mail.outbox), 0)
         print(">> test_alert_email_not_sent_when__get_book_list_with_valid_page_time_to_response_as_expected: OK <<")
 
-    @override_settings(SLOW_API_ALERT_AT_MS=1)
-    def test_alert_email_sent_when_get_book_list_with_valid_page_took_too_much_time_to_response(self):
+    @override_settings(SLOW_API_ALERT_AT_MS=0.001)
+    def test_alert_email_sent_when_get_book_list_with_valid_page_took_time_to_response_more_than_expected(self):
         self.get_book_list_first_page()
 
         self.assertResponseSuccess()
         self.assertEqual(len(mail.outbox), 1)
-        self.assertEqual(mail.outbox[0].subject, "[Alert] Slow API detected")
-        print(">> test_alert_email_sent_when_get_book_list_with_valid_page_took_too_much_time_to_response: OK <<")
+        alert_email = mail.outbox[0]
+        self.assertEqual(alert_email.subject, "[Alert] Slow API detected")
+        self.assertIn(self.staff_user.email, alert_email.to)
+        print(">> test_alert_email_sent_when_get_book_list_with_valid_page_took_time_to_response_more_than_expected: "
+              "OK <<")
