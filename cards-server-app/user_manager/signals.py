156a6f88d54380e8d778bc0a6f5c1d2fc15313ab
Naveya Jintaya <naveya@divertise.asis>
2023-02-14 14:16:05 +0700

    Create function auto create user profile immediately after social auth get extra_data.

--- cards-server-app/user_manager/signals.py
+++ cards-server-app/user_manager/signals.py
@@ -1,10 +1,13 @@
 from django.conf import settings
 from django.core.mail import EmailMultiAlternatives
+from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.template.loader import render_to_string
 from django_rest_passwordreset.signals import reset_password_token_created
+from social_django.models import UserSocialAuth
 
 from config.helpers import get_no_reply_email
+from user_manager.models import Profile
 
 FRONTEND_URL = getattr(settings, "FRONTEND_URL", "")
 DEFAULT_FROM_EMAIL = get_no_reply_email()
@@ -53,3 +56,20 @@ def password_reset_token_created(
     )
     msg.attach_alternative(email_html_message, "text/html")
     msg.send()
+
+
+@receiver(post_save, sender=UserSocialAuth, dispatch_uid="create_profile_for_google_auth")
+def create_profile_for_google_auth(sender, instance, created, **kwargs):
+    # social_django will request extra_data from google after UserSocialAuth has been created.
+    # for google provider, if extra_data has "access_token" it does mean that request extra_data has been called.
+    if (
+        not instance.user.get_profile()
+        and instance.provider == "google-oauth2"
+        and instance.extra_data.get("access_token")
+    ):
+        extra_data = instance.extra_data
+        _, _ = Profile.objects.create(
+            user=instance.user,
+            first_name=extra_data.get("given_name", "Unknown"),
+            last_name=extra_data.get("family_name", "Unknown"),
+        )
