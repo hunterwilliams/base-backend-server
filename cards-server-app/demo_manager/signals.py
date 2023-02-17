6f6c7697c0983dad7a04edc308d06071fa698d76
Naveya Jintaya <naveya@divertise.asis>
2023-02-17 15:33:54 +0700

    Add example logging failed api alert in demo_manager.

--- cards-server-app/demo_manager/signals.py
+++ cards-server-app/demo_manager/signals.py
@@ -1,9 +1,12 @@
+import logging
+
 from django.contrib.auth import get_user_model
 from django.core import mail
 from django.dispatch import receiver
 from django.template.loader import render_to_string
 
 from config.helpers import get_no_reply_email
+from config.middleware.failed_api import failed_api_alert_triggered
 from config.middleware.slow_api import slow_api_alert_triggered
 
 User = get_user_model()
@@ -21,3 +24,8 @@ def handle_slow_api_alert_triggered(sender, alert_data, *args, **kwargs):
         html_message=render_to_string("./email/slow_api_alert/slow_api_alert.html", alert_data),
         fail_silently=False,
     )
+
+
+@receiver(failed_api_alert_triggered)
+def handle_failed_api_alert_triggered(sender, request, request_body, response, alert_data, *args, **kwargs):
+    logger.warning(f"WARNING: Failed API detected {response.status_code} {request.method} {request.path} alert_data: {alert_data}")
